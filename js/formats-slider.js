e12287f1adad488e85dbafc69d62875ecf4d7a7e
liblili <smaragdovna@gmail.com>
2025-11-16 20:15:08 +0100

    Обновил стили, HTML и добавил новый JS и SCSS файлы

---
+++ js/formats-slider.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+
+  navigation: {
+    nextEl: '.carousel-buton.next',
+    prevEl: '.carousel-buton.prev',
+  },
+});
