de4e2aa9fc85ff5f6c86f97d0478ab22209a1c92
liblili <smaragdovna@gmail.com>
2025-11-18 18:53:13 +0100

    newй JS и SCSS

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,29 @@
-const mobileMenu = document.querySelector(`.mobile-menu`);
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+
+// функция открытия/закрытия меню
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+
+// функции открытия и закрытия (чтобы использовать на ссылках)
+const openMenu = () => mobileMenu.classList.add('is-open');
+const closeMenu = () => mobileMenu.classList.remove('is-open');
+
+// кнопки
+menuBtnOpen.addEventListener('click', openMenu);
+menuBtnClose.addEventListener('click', closeMenu);
+
+// ссылки внутри меню
+const menuLinks = document.querySelectorAll('.mobile-menu__item');
+menuLinks.forEach(link => {
+  link.addEventListener('click', closeMenu); // теперь точно работает
+});
+
+/*const mobileMenu = document.querySelector(`.mobile-menu`);
 const menuBtnOpen = document.querySelector(`.menu-btn-open`);
 const menuBtnClose = document.querySelector(`.menu-btn-close`);
 
 const toggleMenu = () => mobileMenu.classList.toggle(`is-open`);
 
 menuBtnOpen.addEventListener(`click`, toggleMenu);
-menuBtnClose.addEventListener(`click`, toggleMenu);
+menuBtnClose.addEventListener(`click`, toggleMenu);*/
